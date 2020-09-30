5cf45581db018f53e65e6400b64dd27fef5754f4
Radosław Zaborski <radex171@gmail.com>
2020-09-30 18:24:02 +0200

    finish project - version for accept client

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
+$(document).ready(function(){
+    $('.carousel-gallery').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: false,
+      touchThreshold: 2,
 
-function clickButton(prop){
-
-        const nameButton = prop;
-        const findArticle = document.querySelector(`.${nameButton}`);
-        const findAllArticle = document.querySelectorAll('article');
-        console.log(findArticle);
-        console.log(findAllArticle);
-
-        for(let article of findAllArticle){
-
-            article.classList.remove('active');
-        }
-
-        findArticle.classList.add('active');
+    });
+  });
+  
+function clickButton(e){
         
-        
+  const nameButton = e;
+  const findArticle = document.querySelector(`.${nameButton}`);
+  const findAllArticle = document.querySelectorAll('article');
+  
+  for(let article of findAllArticle){
+
+      article.classList.remove('active');
+  }
+
+  findArticle.classList.add('active');
 };
-
